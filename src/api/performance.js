79d5a63a7aa80c7e931f1e1f5b81eb6dc9d4292b
hh613 <2023110553@dgu.ac.kr>
2026-09-22 01:12:54 +0900

    fix/#153-공연-API-경로-수정

--- src/api/performance.js
+++ src/api/performance.js
@@ -1,6 +1,6 @@
 import { apiClient } from './client'
 
-export const getPerformances = (date) => apiClient.get('/api/performances', { params: { date } })
-export const getPerformanceDetail = (performanceId) => apiClient.get(`/api/performances/${performanceId}`)
+export const getPerformances = (date) => apiClient.get('/api/performances/', { params: { date } })
+export const getPerformanceDetail = (performanceId) => apiClient.get(`/api/performances/${performanceId}/`)
 export const getNowPerformances = () =>
-    apiClient.get('/api/performances/now')
+    apiClient.get('/api/performances/now/')
